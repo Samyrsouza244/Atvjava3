3f747470ec1c492ac5d76719826067be0123b4ec
Samyrsouza244 <samyr1479ee@gmail.com>
2022-07-12 12:08:20 -0300

    desaio entregue

---
+++ script.js
@@ -0,0 +1,23 @@
+let year = 19
+if (year>=18){console.log("você é maior")}else {console.log("você não é maior")}
+
+let newAge= 18;
+let humano=true
+if((newAge=18) && (humano=true)){
+    console.log('liberado')
+}else{
+    console.log('nao ta liberado')
+
+} let name='R';
+if(name === 'R'){
+    console.log('Meu nome começa com a letra R')
+} else{
+    console.log('Meu nome não começa com a letra R')
+}
+
+let lastName= 'Souza';
+if(lastName.length>=6 || lastName.substring(0,1) == 'E'){
+    console.log('Seu sobrenome tem 6 letras e seu nome começa com E')
+}else{
+    console.log('seu nome nao tem 6 letras e seu nome nao começa com a letra E')
+}
